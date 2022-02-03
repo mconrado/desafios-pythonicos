9ab2f1f7e53629a124da0f52fbddc2da476f7367
mconrado <m.conrad_reis@hotmail.com>
2022-02-03 17:23:43 -0300

    segunda solução: usando for puramente

--- 10_sort_last.py
+++ 10_sort_last.py
@@ -11,8 +11,23 @@ Dica: Use uma custom key= function para extrair o ultimo elemento de cada tupla.
 """
 def sort_last(tuples):
     # +++ SUA SOLUÇÃO +++
-    return sorted(tuples, key=lambda x: x[-1])
+    propertysToSort = []
+    result = []
 
+    for item in tuples:
+        propertysToSort.append(item[-1])
+    
+    propertysToSort.sort()    
+    
+    for itemSort in propertysToSort :
+        for itemTuple in tuples :
+            if itemTuple[-1] == itemSort :
+                result.append(itemTuple)
+            
+    
+
+
+    return result
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
