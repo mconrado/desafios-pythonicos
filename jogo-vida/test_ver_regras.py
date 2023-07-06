5543bb372537a68e9a903fe87bed1cfd9a778831
mconrado <m.conrad_reis@hotmail.com>
2023-07-06 16:34:09 -0300

    refatorando ver regras

--- jogo-vida/test_ver_regras.py
+++ jogo-vida/test_ver_regras.py
@@ -9,6 +9,7 @@ matrix2 = [[1, 1, 0], [1, 0, 1], [0, 1, 0]]
 def test_ver_regras():  
   assert ver_regras(1, vizinhos(matrix0, 1, 1)) == 0
   assert ver_regras(1, vizinhos(matrix1, 2, 1)) == 0
+  assert ver_regras(1, vizinhos(matrix2, 0, 0)) == 1
 
 if __name__ == "__main__":
     pytest.main(['-svv', __file__])
--- jogo-vida/ver_regras.py
+++ jogo-vida/ver_regras.py
@@ -3,7 +3,7 @@ def ver_regras(celula, vizinhos):
 
     estado_celula = 0
 
-    permanece_viva = celula == 1 and (qt_vizinhos_vivos == 2 or qt_vizinhos_vivos == 3)
+    permanece_viva = celula == 1 and (qt_vizinhos_vivos in (2, 3))
     ressucita = celula == 0 and qt_vizinhos_vivos == 3
 
     if permanece_viva or ressucita:
